40cdcece1a31b8cb12f2d1bccad248c7bf570dab
Mohammad Jubair <you@example.com>
2023-03-15 10:46:17 +0600

    range button done

--- rooms.js
+++ rooms.js
@@ -1,20 +1,27 @@
-let allRooms=[]
+let allRooms = [];
 
 const fetchRoomsData = async () => {
   const data = await fetch(`ROOMS.json`);
   const result = await data.json();
-  allRooms=result
+  allRooms = result;
   displayRoomsData(result);
 };
 
 fetchRoomsData();
 
 const displayRoomsData = async (rooms) => {
-  
   const roomsContainer = document.getElementById("rooms-container");
-  roomsContainer.innerHTML=''
+  roomsContainer.innerHTML = "";
   rooms.forEach((room) => {
-    const { name, summary, property_type, images,number_of_reviews,price,_id } = room;
+    const {
+      name,
+      summary,
+      property_type,
+      images,
+      number_of_reviews,
+      price,
+      _id,
+    } = room;
     roomsContainer.innerHTML += `
  <div class="col">
  <div class="card h-100">
@@ -36,27 +43,19 @@ const displayRoomsData = async (rooms) => {
   });
 };
 
-
-
-
-
 const range = document.getElementById("review-range");
 range.addEventListener("input", () => {
-  const value = range.Value;
+  const value = range.value;
 
-  document.getElementById('review-count').innerText = value
-  const filteredData= allRooms.filter( r.number_of_reviews >= value)
-  displayRoomsData(allRooms) 
+  document.getElementById("review-count").innerText = value;
+  const filteredData = allRooms.filter((r) => r.number_of_reviews >= value);
+  displayRoomsData(filteredData);
 });
 
-
-
-
-
-document.getElementById('sort-by-price-btn').addEventListener('click', () =>{
-     allRooms.sort((a,b)=>{
-        return parseFloat(a.price) < parseFloat(b.price)  ? 1: -1
-    })
-    // console.log(allRooms)
-    displayRoomsData(allRooms)
-})
+document.getElementById("sort-by-price-btn").addEventListener("click", () => {
+  allRooms.sort((a, b) => {
+    return parseFloat(a.price) < parseFloat(b.price) ? 1 : -1;
+  });
+  // console.log(allRooms)
+  displayRoomsData(allRooms);
+});
